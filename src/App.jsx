6f70ff38a0e8d0165222c4e29cdf068d540c5c98
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-03-31 23:33:09 +0100

    fetch and display pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pokemon, setPokemon] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return <></>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+        const data = await res.json();
+
+        const simplePokemonData = data.results.map((pokemon, i) => {
+          const id = i + 1;
+          return {
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          };
+        });
+        setPokemon(simplePokemonData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className="place-items-center">
+      <img src="/pokemon-23.svg" alt="Pokemon Logo" className="w-100 my-10" />
+      {isLoading ? (
+        <h1 className="text-4xl mt-50 font-bold">Loading...</h1>
+      ) : (
+        <div className="bg-gray-200 min-h-[500px] max-w-[900px] place-items-center p-5 m-5 rounded-xl">
+          <p className="bg-white p-4 rounded-md">
+            You can only select one of each or else you lose!
+          </p>
+          <div className="flex flex-wrap place-content-center gap-2 mt-5">
+            {" "}
+            {pokemon.map((p, i) => {
+              return <Card {...p} key={i} />;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div
+      value={props.name}
+      className="place-items-center p-4 bg-white rounded-xl cursor-pointer hover:brightness-95 active:scale-95"
+    >
+      <h2>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</h2>
+      <img src={props.sprite} alt={props.name} />
+    </div>
+  );
+}
+
+export default Card;
